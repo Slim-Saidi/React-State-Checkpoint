c6d01a5d39badd984d546ec04f10505aa5f94f62
slim-saidi <slim-saidi@hotmail.com>
2020-12-06 11:53:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import Pic from "./mayweather.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        name: 'Floyd "Money" Mayweather',
+        bio: "The Best Ever",
+        profession: "Professional Boxer",
+      },
+      check: false,
+      count: 0,
+    };
+  }
+
+  show = () => {
+    this.setState({
+      check: !this.state.check,
+    });
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ count: this.state.count + 1 });
+    }, 1000);
+  }
+
+  render() {
+    const { count } = this.state;
+    return (
+      <div className="App">
+        <h1>Boxing Hall Of Fame</h1>
+        <div> {count} </div>
+        <br />
+        <button onClick={this.show}>Check</button>
+        <br />
+        {this.state.check ? (
+          <div>
+            <div>
+              <img src={Pic} alt={""} width={300} />
+            </div>
+            <h2> {this.state.person.name} </h2>
+            <h3> {this.state.person.bio} </h3>
+            <h3> {this.state.person.profession} </h3>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default App;
